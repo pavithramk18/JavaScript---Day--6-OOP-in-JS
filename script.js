e1eb2c7e2dab9f601b9092838153907b1863baec
pavithramk18 <pavithram.kumaresan@gmail.com>
2023-10-16 21:33:34 +0530

    changes

--- script.js
+++ script.js
@@ -15,58 +15,84 @@ console.log("JavaScript - Day -6 OOP in JS");
 
 // a) Write a constructor for the class Movie, which takes a String representing the title of the movie, a String representing the studio, and a String representing the rating as its arguments, and sets the respective class properties to these values.
 
-class Movie{
-    constructor(title,studio,rating){
-        this.title=title;
-        this.studio=studio;
-        this.rating=rating;
-    }
-}
+// class Movie{
+//     constructor(title,studio,rating){
+//         this.title=title;
+//         this.studio=studio;
+//         this.rating=rating;
+//     }
+// }
 
-var show=new Movie("Cindrella","Fox Studios","PG 13,R");
-console.log("Title"+": "+show.title);
-console.log("Show:"+" "+show.studio);
-console.log("Rating"+":"+show.rating);
+// var show=new Movie("Cindrella","Fox Studios","PG 13,R");
+// console.log("Title"+": "+show.title);
+// console.log("Show:"+" "+show.studio);
+// console.log("Rating"+":"+show.rating);
 
-//b) The constructor for the class Movie will set the class property rating to "PG"
-// as default when no rating is provided.
+// //b) The constructor for the class Movie will set the class property rating to "PG"
+// // as default when no rating is provided.
 
-class Movie{
-    constructor(title,studio,rating="PG"){
-        this.title=title;
-        this.studio=studio;
-        this.rating=rating;
-    }
-}
+// class Movie{
+//     constructor(title,studio,rating="PG"){
+//         this.title=title;
+//         this.studio=studio;
+//         this.rating=rating;
+//     }
+// }
 
-var show=new Movie("Cindrella","Fox Studios");
-console.log("Title"+": "+show.title);
-console.log("Show:"+" "+show.studio);
-console.log("Rating"+":"+show.rating);
+// var show=new Movie("Cindrella","Fox Studios");
+// console.log("Title"+": "+show.title);
+// console.log("Show:"+" "+show.studio);
+// console.log("Rating"+":"+show.rating);
 
 //c) Write a method getPG, which takes an array of base type Movie as its argument, and
 //returns a new array of only those movies in the input array with a rating of "PG".
 //You may assume the input array is full of Movie instances. The returned array need not be full.
 
-
-
+class Movie {
+    constructor(title, rating,studio) {
+      this.title = title;
+      this.rating = rating;
+      this.studio=studio;
+    }
+  }
+  
+  function getPG(movieArray) {
+    const pgMovies = [];
+    for (let i = 0; i < movieArray.length; i++) {
+      if (movieArray[i].rating === 'PG') {
+        pgMovies.push(movieArray[i]);
+      }
+    }
+    return pgMovies;
+  }
+  
+  // Example usage:
+  const movies = [
+    new Movie('Movie 1', 'PG',"Fox Studio"),
+    new Movie('Movie 2', 'R',"2D Entertainment"),
+    new Movie('Movie 3', 'PG',"Red Giant"),
+    new Movie('Movie 4', 'G',"Siva Productions")
+  ];
+  
+  const pgMovies = getPG(movies);
+  console.log(pgMovies);
 
 //d) Write a piece of code that creates an instance of the class Movie
 // with the title “Casino Royale”, the studio “Eon Productions”, and the rating “PG­13”
 
-class Movie {
-  constructor(title, studio, rating) {
-    this.title = title;
-    this.rating = rating;
-    this.studio = studio;
-  }
+// class Movie {
+//   constructor(title, studio, rating) {
+//     this.title = title;
+//     this.rating = rating;
+//     this.studio = studio;
+//   }
 
-  moviedetails() {
-    return `Title:  ${this.title}
-        Studio:${this.studio}
-                Rating :${this.rating}`;
-  }
-}
+//   moviedetails() {
+//     return `Title:  ${this.title}
+//         Studio:${this.studio}
+//                 Rating :${this.rating}`;
+//   }
+// }
 
-var m = new Movie("Casino Royal", "Eon Productions", "PG­13");
-console.log(m.moviedetails());
+// var m = new Movie("Casino Royal", "Eon Productions", "PG­13");
+// console.log(m.moviedetails());
